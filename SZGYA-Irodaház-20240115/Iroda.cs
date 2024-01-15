346b9d023692b0c9b3cf338061801591d8755604
Csizmadia Márk <kindmysticaltree@gmail.com>
2024-01-15 14:06:30 +0100

    Add project files.

---
+++ SZGYA-Irodaház-20240115/Iroda.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZGYA_Irodahaz_20240115
+{
+    internal class Iroda
+    {
+        public string Kod { get; set; }
+        public int Kezdet { get; set; }
+        List<int> LetszamAdat { get; set; }
+
+        public Iroda(string sor) 
+        {
+            this.LetszamAdat = new List<int>(); 
+            string[] adatok = sor.Split(' ');
+            this.Kod = adatok[0];
+            this.Kezdet = int.Parse(adatok[1]);
+            for (int i = 2; i < adatok.Length; i++)
+            {
+                this.LetszamAdat.Add(int.Parse(adatok[i]));
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Kod, -15} {this.Kezdet, 6} {string.Join(' ', this.LetszamAdat)}";
+        }
+    }
+}
---
+++ SZGYA-Irodaház-20240115/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZGYA_Irodahaz_20240115
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Iroda> irodak = new List<Iroda>();
+
+            StreamReader sr = new StreamReader("../../../src/irodahaz.txt", Encoding.UTF8);
+
+            while(!sr.EndOfStream)
+            {
+                irodak.Add(new Iroda(sr.ReadLine()));
+            }
+
+            Console.WriteLine($"{"SORSZÁM",7} {"KÓD", -15} {"KEZDET", 6} Létszámadatok");
+
+            for (int i = 0; i < irodak.Count; i++)
+            {
+                Console.WriteLine($"{i + 1,6} {irodak[i]}");
+            }
+
+            //8.feladat
+            Console.WriteLine($"{irodak.MaxBy(i => i.LetszamAdat.)}");
+        }
+    }
+}
