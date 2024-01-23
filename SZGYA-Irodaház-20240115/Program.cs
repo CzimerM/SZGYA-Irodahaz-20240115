9b0105c5995a0b37c64591b0423a994dae8ba1dc
Csizmadia Márk <airamek@teamorange.hu>
2024-01-23 13:15:25 +0100

    kész

--- SZGYA-Irodaház-20240115/Program.cs
+++ SZGYA-Irodaház-20240115/Program.cs
@@ -24,9 +24,9 @@ namespace SZGYA_Irodahaz_20240115
 
             Console.WriteLine($"{"SORSZÁM",7} {"KÓD", -15} {"KEZDET", 6} Létszámadatok");
 
-            for (int i = 0; i < irodak.Count; i++)
+            foreach (var i in irodak)
             {
-                Console.WriteLine($"{i + 1,6} {irodak[i]}");
+                Console.WriteLine($"{i.Sorszam,7} {i}");
             }
 
             //8.feladat
@@ -37,7 +37,7 @@ namespace SZGYA_Irodahaz_20240115
             Console.WriteLine("\n9.feladat");
             Iroda kilencFo = irodak.FirstOrDefault(i => i.LetszamAdat.Contains(9));
             if (kilencFo == null) Console.WriteLine("[HIBA] Nincs ilyen");
-            else Console.WriteLine(kilencFo);
+            else Console.WriteLine("Van ilyen iroda");
 
             //10.feladat
             Console.WriteLine("\n10.feladat");
@@ -46,11 +46,27 @@ namespace SZGYA_Irodahaz_20240115
             //11.feladat
             Console.WriteLine("\n11.feladat");
             StreamWriter sw = new StreamWriter("../../../otnelKevesebb.txt", false, Encoding.UTF8);
-            foreach (var i in irodak.Where(i => i.LetszamAdat.Contains(0)))
-            {
-                sw.WriteLine($"{i.Kod} {string.Join(' ', i.LetszamAdat)}");
-            }
-            
+
+            irodak.Where(i => i.LetszamAdat.Contains(0)).ToList().ForEach(i => sw.WriteLine($"{i.Kod} {string.Join('/', i.LetszamAdat.Select((item, index) => (item, index)).Where(i => i.item == 0).Select(i => i.index + 1))}"));
+       
+            //12.feladat
+            Console.WriteLine($"\n12.feladat: LOGMEIN átlag dolgozószáma egy irodában: {Math.Round(irodak.Where(i => i.Kod == "LOGMEIN").First().LetszamAdat.Average())}");
+
+            //13.feladat
+            Console.WriteLine("\n13.feladat");
+            sw.WriteLine("EMELET SORSZÁMA | DOLGOZÓK SZÁMA");
+            irodak.ForEach(i => sw.WriteLine($"{i.Sorszam, 15} | {i.LetszamAdat.Sum()}"));
+
+            sw.Close();
+
+            //14.feladat
+            Console.WriteLine($"\n14.feladat: {irodak.Sum(i => i.LetszamAdat.Sum())} ember dolgozik összesen az irodaházban.");
+
+            //15.feladat
+            Console.WriteLine($"\n15.feladat: {irodak.Min(i => i.Kezdet)}");
+
+            //16.feladat
+            Console.WriteLine($"\n16.feladat: {DateTime.Now.Year - irodak.Max(i => i.Kezdet)}");
         }
     }
 }
